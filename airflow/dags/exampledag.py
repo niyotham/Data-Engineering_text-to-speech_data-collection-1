eb4527a8b23b337429abfcaed910122cd35fedd4
emtinanseo <emtinan.s.e.osman@gmail.com>
2022-10-05 19:35:47 +0000

    example dag

--- airflow/dags/exampledag.py
+++ airflow/dags/exampledag.py
@@ -1,3 +1,19 @@
-from airflow import DAG
 
-print("this is a check for airflow")
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+#print("this is a check for airflow")
+with DAG(
+    dag_id ='example_dag',
+    description = "Example Dag",
+    start_date = datetime(2022,10,4,10),
+    schedule_interval = '@daily'
+) as dag:
+#A task bash operator = runs bash command, python operator = runs python code
+    task1 = BashOperator(
+        task_id = 'task_bash_operator_1',
+        bash_command= "echo 'Hello World!'"
+    )
+    task1
