3336b432ef77d7d4ede52976259673983e6ca5a7
Fisseha Linux <fisseha.137@gmail.com>
2022-10-06 00:52:26 +0300

    using the AWS kafka broker

--- kafka-python-getting-started/py_consumer.py
+++ kafka-python-getting-started/py_consumer.py
@@ -2,10 +2,13 @@ from kafka import KafkaConsumer
 import json
 import msgpack as pk
 
+
+aws_instance_bootstrap_servers = ['b-1.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092',
+                                  'b-2.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092']
 # To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer('my-topic',
-                         group_id='my-group',
-                         bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer('g1-voices-test-topic',
+                         group_id='my-group-g1',
+                         bootstrap_servers=aws_instance_bootstrap_servers)
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
