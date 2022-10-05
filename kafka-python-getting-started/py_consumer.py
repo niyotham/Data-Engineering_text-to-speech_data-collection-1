d6718346eca660adee5ad704056693df15e4706e
Fisseha Linux <fisseha.137@gmail.com>
2022-10-05 10:55:10 +0300

    pep 8: 80%: consumer

--- kafka-python-getting-started/py_consumer.py
+++ kafka-python-getting-started/py_consumer.py
@@ -9,9 +9,9 @@ consumer = KafkaConsumer('my-topic',
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                         message.offset, message.key,
+                                         message.value))
 
 # consume earliest available messages, don't commit offsets
 KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False)
