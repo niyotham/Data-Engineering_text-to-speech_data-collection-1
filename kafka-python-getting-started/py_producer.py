ec0f3d4ce5a7d8ae58895647cdd313e68af8861c
fisseha estifanos <fisseha.137@gmail.com>
2022-10-06 20:16:22 +0000

    minor updates

--- kafka-python-getting-started/py_producer.py
+++ kafka-python-getting-started/py_producer.py
@@ -1,16 +1,18 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 import msgpack as pk
-# import json
+import json
 
 
+topic_name = 'g1-test-topic'
+bootstrap_server = ['localhost:9092']
 aws_instance_bootstrap_servers = ['b-1.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092',
                                   'b-2.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092']
 
 producer = KafkaProducer(bootstrap_servers=aws_instance_bootstrap_servers)
 
 # Asynchronous by default
-future = producer.send('g1-voices-test-topic', b'raw_bytes')
+future = producer.send(topic_name, b'raw_bytes')
 
 # Block for 'synchronous' sends
 try:
@@ -26,21 +28,20 @@ print(record_metadata.partition)
 print(record_metadata.offset)
 
 # produce keyed messages to enable hashed partitioning
-producer.send('g1-voices-test-topic', key=b'foo', value=b'bar')
+producer.send(topic_name, key=b'foo', value=b'bar')
 
 # encode objects via msgpack
 producer = KafkaProducer(value_serializer=pk.dumps)
 producer.send('msgpack-topic', {'key': 'value'})
 
 # produce json messages
-# TODO: the line below was not commented
-# TODO: https://bobbyhadz.com/blog/python-typeerror-object-of-type-bytes-is-not-json-serializable#:~:text=The%20Python%20%22TypeError%3A%20Object%20of,string%20before%20serializing%20to%20JSON.
-# producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).decode('utf-8').encode('utf-8'))
+producer = KafkaProducer(
+    value_serializer=lambda m: json.dumps(m).encode('ASCII'))
 producer.send('json-topic', {'key': 'value'})
 
 # produce asynchronously
 for _ in range(100):
-    producer.send('g1-voices-test-topic', b'msg')
+    producer.send(topic_name, b'msg')
 
 
 def on_send_success(record_metadata):
@@ -55,8 +56,8 @@ def on_send_error(excp):
 
 
 # produce asynchronously with callbacks
-producer.send('g1-voices-test-topic',
-              b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
+producer.send(
+    topic_name, b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
 
 # block until all async messages are sent
 producer.flush()
