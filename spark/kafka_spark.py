9c476daeb0e5d1cb7c2ee5d7182be482fe96b60a
Niyomukiza Thamar <thamarniyo@gmail.com>
2022-10-06 09:35:25 +0200

    spark init

---
+++ spark/kafka_spark.py
@@ -0,0 +1,48 @@
+import os
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, IntegerType, \
+    StringType, FloatType, TimestampType
+
+audio_data = "/mnt/10ac-batch-6/week7/chang/kafka"
+topic_input = "test"
+
+import findspark
+findspark.init('/opt/spark')
+
+import os
+os.environ['PYSPARK_SUBMIT_ARGS'] =\
+     '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 pyspark-shell'
+
+
+
+def main():
+
+    spark = SparkSession \
+        .builder \
+        .master('local[1]') \
+        .appName("kafka-streaming-audio") \
+        .getOrCreate()
+
+    df_audio = read_from_kafka(spark)
+
+    # summarize_sales(df_audio)
+
+
+def read_from_kafka(spark):#, params):
+    options_read = {
+        "kafka.bootstrap.servers":
+            "localhost:9092",
+        "subscribe":
+            topic_input,
+    }
+
+    df_audio = spark.readStream \
+        .format("kafka") \
+        .options(**options_read) \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    return df_audio
+
+
