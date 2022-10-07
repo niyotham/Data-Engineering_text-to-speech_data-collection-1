75630619e0887e16554a6aea8da8605c8d350627
fisseha estifanos <fisseha.137@gmail.com>
2022-10-07 19:24:06 +0000

    simple test helper

--- main-pipeline/helper.py
+++ main-pipeline/helper.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import json
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
 
 
 def produce_the_message():
-
-
     raw_data_ = pd.read_csv('../data/raw_data.csv')
     print(raw_data_.shape)
 
@@ -14,7 +13,7 @@ def produce_the_message():
 
     print('publishing raw message . . .')
     # raw_data = ti.xcom_pull(key='raw_data_from_csv', task_ids='raw_data_read')
-    
+
     # TODO" uncomment the next two lines to print data info
     #print(f'data: {raw_data}\ntype of the data: {type(raw_data)}')
     #print(f'using json.dumps: {json.dumps(raw_data, indent=4)}')
@@ -29,10 +28,26 @@ def produce_the_message():
     # producer = KafkaProducer(
     #    value_serializer=lambda v: json.dumps(v).encode("utf-8"))
 
-    print(f"article: {raw_data['article']}")
-    print(f"keys: {type(raw_data['article'].keys())}")
-    print(f"values: {type(raw_data['article'].values())}")
-    #producer.send(RAW_DATA_TOPIC, {"text": raw_data['data'][0]})
+    # print(f"article: {raw_data['article']}")
+    # print(f"keys: {raw_data['article'].keys()}")
+    # print(f"values: {raw_data['article'].values()}")
+    for i in range(0, 3):
+        print(f"{i}: {type(raw_data['article'][i])}")
+        print(raw_data['article'][i])
+        # producer.send(RAW_DATA_TOPIC, key=i, value=raw_data['article'][i])
+        future = producer.send(RAW_DATA_TOPIC, b'test message . . . ')
+        try:
+            record_metadata = future.get(timeout=10)
+        except KafkaError:
+            # Decide what to do if produce request failed...
+            log.exception()
+            pass
+
+        # Successful result returns assigned partition and offset
+        print(record_metadata.topic)
+        print(record_metadata.partition)
+        print(record_metadata.offset)
+
     print('publishing raw messages completed. . .')
 
 
