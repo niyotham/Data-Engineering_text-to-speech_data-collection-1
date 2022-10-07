44c9e56da867ccdff775ccc4e956f3d59531d4a5
Natty <natnaelmasresha@gmail.com>
2022-10-07 17:20:11 +0000

    change models data type

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
-class AudioData:
+from pydantic import BaseModel
+from fastapi import File, UploadFile
+class AudioData(BaseModel):
     id:str
-    audio:bytearray
+    audio:UploadFile= File()
+    class Config:
+        arbitrary_types_allowed = True
